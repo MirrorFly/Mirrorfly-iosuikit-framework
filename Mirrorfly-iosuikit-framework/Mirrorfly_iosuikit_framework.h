711d17296918651094243a6e3d12363ef0044753
Gurudharan <gurudharan.a@contus.in>
2023-05-17 10:34:30 +0530

    missing files

---
+++ Mirrorfly-iosuikit-framework/Mirrorfly_iosuikit_framework.h
@@ -0,0 +1,18 @@
+//
+//  Mirrorfly_iosuikit_framework.h
+//  Mirrorfly-iosuikit-framework
+//
+//  Created by Gurudharan on 06/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Mirrorfly_iosuikit_framework.
+FOUNDATION_EXPORT double Mirrorfly_iosuikit_frameworkVersionNumber;
+
+//! Project version string for Mirrorfly_iosuikit_framework.
+FOUNDATION_EXPORT const unsigned char Mirrorfly_iosuikit_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mirrorfly_iosuikit_framework/PublicHeader.h>
+
+
